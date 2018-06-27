ebf127942a769226ee45be33341959c1cd587481
Hung Tran <hung.tran@swissitbridge.com>
2018-06-27 15:01:28 +0700

    commit

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './Person.css';
+
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
             {/* show the content placed between the opening & closing tag of the component */}
             <p>{props.children}</p>
